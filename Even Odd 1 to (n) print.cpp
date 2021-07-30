d7c2431c89d2ac3f8195c341316868a0d6c2e6b5
HASNAT JANI RAFIN <77789741+HasnatJaniRafin@users.noreply.github.com>
2021-07-30 14:31:51 +0800

    Create Even Odd 1 to (n) print.cpp

---
+++ Even Odd 1 to (n) print.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std ;
+int main ( )
+{
+    
+    int n ;
+    cout <<" Enter number :" ;
+    cin >>n ;
+    
+    for (int i=1 ; i<=n ; i++){
+        
+    
+    (i %2==0) ? (cout <<i  <<" is even number " <<endl ) : (cout <<i  <<" is odd number " <<endl) ;
+    
+    }
+    
+    
+    
+    
+    
+    
+} 
